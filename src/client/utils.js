7f707335bf92c8f4830be54b649784b351c1d06a
Jonathan Dart <jonathandart@gmail.com>
2015-11-22 21:10:28 -0600

    removed old image preloading code

--- src/client/utils.js
+++ src/client/utils.js
@@ -9,27 +9,3 @@ export function hostMatch(host, candidate) {
   return host === candidateLevels.slice(-hostLevels.length).join('.');
 }
 
-const imgState = {
-  working: false,
-  queue: []
-};
-
-export function imgPreload(url) {
-  imgState.queue.push(url);
-  if (imgState.working)
-    return;
-  imgLoop();
-}
-
-function imgLoop() {
-  if (!imgState.queue.length) {
-    imgState.working = false
-    return;
-  }
-  imgState.working = true;
-  const next = imgState.queue.shift();
-  const img = new Image();
-  img.onload = setTimeout(imgLoop, 50);
-  img.src = next;
-}
-
