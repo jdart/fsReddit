5578a8350d664fd54cdf80b0d300c4559e9c351c
Jonathan Dart <jonathandart@gmail.com>
2016-04-09 10:16:17 -0500

    cleanup

--- src/server/frontend/index.js
+++ src/server/frontend/index.js
@@ -16,10 +16,6 @@ app.use(compression());
 app.use('/assets/img', express.static('../assets/img', {maxAge: '200d'}));
 app.use('/_assets', express.static('build', {maxAge: '200d'}));
 
-// Intl.
-app.use('/node_modules/intl/dist', express.static('node_modules/intl/dist'));
-app.use('/node_modules/intl/locale-data', express.static('node_modules/intl/locale-data'));
-
 app.use(device.capture());
 app.get('*', render);
 
